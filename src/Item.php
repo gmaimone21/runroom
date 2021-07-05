c2905ff3271335e7d213be33d8c5f4c39a2a48da
g.maimone <g.maimone@kentech-sp.com>
2021-07-05 02:36:57 +0100

    More PSR rules adaptation - typed php missing returning types

--- src/Item.php
+++ src/Item.php
@@ -9,14 +9,14 @@ class Item
     public int $sellIn;
     public int $quality;
 
-    function __construct($name, $sellIn, $quality)
+    public function __construct(string $name, int $sellIn, int $quality)
     {
         $this->name = $name;
         $this->sellIn = $sellIn;
         $this->quality = $quality;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return "{$this->name}, {$this->sellIn}, {$this->quality}";
     }
